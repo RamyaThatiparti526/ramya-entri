82e8ed5fd5f527d4078ff7aaf102ed546670ee94
Ramya <ramyathatiparti@gmail.com>
2023-08-18 08:37:16 +0530

    added files

---
+++ javascript/basics.js
@@ -0,0 +1,11 @@
+//let x=5;
+//let y=7;
+//console.log(x+y);
+//console.log('ramya')
+
+
+let x;y;z;
+x=5;
+y=2;
+z=11;
+console.log(x+y+z);
---
+++ javascript/task1.js
@@ -0,0 +1,9 @@
+let x=5;
+let y=7;
+console.log("addition", x+y);
+console.log("subtraction", x-y);
+console.log("multiplication", x*y);
+console.log("remainder",x%y);
+console.log("division",x/y);
+console.log("exponent", x**y);
+console.log("quotient", Math.floor(x / y));
