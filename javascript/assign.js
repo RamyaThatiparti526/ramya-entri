b9c97e3f1c57f567fc9c8f5518f6ae04563e3d54
Ramya <ramyathatiparti@gmail.com>
2023-08-22 15:51:08 +0530

    adding file

---
+++ javascript/assign.js
@@ -0,0 +1,5 @@
+const name1 ='Robin singh';
+const truncate_string= name1.slice(0,4);
+console.log(truncate_string);
+const truncate_string1=name1.slice(6);
+console.log(truncate_string1);
--- javascript/basics.js
+++ javascript/basics.js
@@ -3,9 +3,13 @@
 //console.log(x+y);
 //console.log('ramya')
 
+  
+//let x;y;z;
+//x=5;
+//y=2;
+//z=11;
+//console.log(x+y+z);
 
-let x;y;z;
-x=5;
-y=2;
-z=11;
-console.log(x+y+z);
+//const mobile=7989686464
+//console.log
+
