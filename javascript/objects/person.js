febae946198f1bd00a95a7cb6233a9c0d4604d75
Ramya <ramyathatiparti@gmail.com>
2023-09-14 22:58:53 +0530

    updated objects

--- javascript/objects/person.js
+++ javascript/objects/person.js
@@ -1,26 +1,26 @@
-// const person={
-//     name:"Ramya thatiparti",
-//     dob:"11.08.1988.",
-//     qualification:"B.tech",
-//     job:" ",
-//     emailid:"ramyathatiparti@gmail.com",
-//     hobbies:
-//     {
-//     sports: "badminton",
-//     listen: "music",
-//    }
-//     }
-//     console.log("name: ",person.name);
-//     console.log(person.dob);
-//     console.log(person. qualification);
-//     console.log(person.job);
-//     console.log(person.emailid);
-//     console.log(person.hobbies.sports);
-//     console.log(person["name"]);
-//     person.fname="thatiparti";
-//     console.log(person);
-//     delete person.dob;
-//     console.log(person);
+const person={
+    name:"Ramya thatiparti",
+    dob:"11.08.1988.",
+    qualification:"B.tech",
+    job:" ",
+    emailid:"ramyathatiparti@gmail.com",
+    hobbies:
+    {
+    sports: "badminton",
+    listen: "music",
+   }
+    }
+    console.log("name: ",person.name);
+    console.log(person.dob);
+    console.log(person. qualification);
+    console.log(person.job);
+    console.log(person.emailid);
+    console.log(person.hobbies.sports);
+    console.log(person["name"]);
+    person.fname="thatiparti";
+    console.log(person);
+    delete person.dob;
+    console.log(person);
 
     const array1=[[1,2,3],[4,5,6],[7,8,9],{name:"Ramya thatiparti",
         dob:"11.08.1988.",
@@ -29,4 +29,7 @@
         emailid:"ramyathatiparti@gmail.com",}];
     console.log(array1[0]);
     console.log(array1[3].name);
+   array1[3].lname="thatiparti";
+   console.log(array1);
+
 
