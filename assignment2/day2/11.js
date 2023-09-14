3605420cd51fab12b5e11c7296fe061a4675bfd6
Ramya <ramyathatiparti@gmail.com>
2023-09-14 20:04:26 +0530

    adding assignment

---
+++ assignment2/day2/11.js
@@ -0,0 +1,10 @@
+
+// 11. Write a function `isAnagram` that takes two strings as input and returns `true` if they are anagrams (contain the same characters in a different order), and `false` otherwise.
+
+//    **Example:**
+//    ```javascript
+   const input1 = "listen";
+   const input2 = "silent";
+   const output = isAnagram(input1, input2);
+   
+   console.log(output); // true
---
+++ assignment2/day2/12.js
@@ -0,0 +1,8 @@
+
+// 12. Write a function `sumArray` that takes an array of numbers as input and returns the sum of all the numbers.
+
+//    **Example:**
+//    ```javascript
+   const input = [1, 2, 3, 4, 5];
+   const output = sumArray(input);
+   console.log(output); // 15
