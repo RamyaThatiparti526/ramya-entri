efa69174a1fa318d8a26726b53e872382278db91
Ramya <ramyathatiparti@gmail.com>
2023-09-15 23:12:30 +0530

    adding files

---
+++ assignment2/day3q/6.js
@@ -0,0 +1,12 @@
+// **Question 6: Merge Two Sorted Arrays**
+
+// Write a function `mergeSortedArrays` that takes two sorted arrays as input and merges them into a single sorted array.
+
+// function mergeSortedArrays(inputArray1,inputArray2){
+
+let inputArray1 = [1, 3, 5];
+let inputArray2 = [2, 4, 6];
+let output =inputArray1+","+inputArray2;
+console.log(output); // [1, 2, 3, 4, 5, 6]
+
+// console.log( mergeSortedArrays(inputArray1,inputArray2));
--- javascript/assign.js
+++ javascript/assign.js
@@ -136,6 +136,12 @@ return'not leap year';
 }
 let year=2003;
 console.log(leapyear(year));
+
+
+
+
+
+
 // 10)Positive, Negative, or Zero
 // Write a function positiveNegativeZero that takes a number as input and prints "Positive" if the
 // number is greater than 0, "Negative" if it's less than 0, and "Zero" if it's equal to 0.
