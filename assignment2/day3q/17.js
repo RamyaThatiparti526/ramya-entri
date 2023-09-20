b8490d0d2e9dd2754a4f9e3c47183229d2d1669f
Ramya <ramyathatiparti@gmail.com>
2023-09-20 13:28:43 +0530

    added file

---
+++ assignment2/day3q/17.js
@@ -0,0 +1,16 @@
+// 17. Write a function `findIndex` that takes an array and an element as input and returns the index of the first occurrence of the element in the array. If the element is not found, return -1.
+
+//     **Example:**
+//     ```javascript
+//     const inputArray = [1, 2, 3, 4, 5];
+//     const element = 3;
+//     const output = findIndex(inputArray, element);
+//     console.log(output); // 2
+
+    function findIndex(arr){
+    const  index =arr.indexOf(i);
+    return index;
+    }
+   let i='apple';
+    let arr=["banana","apple","cherry"];
+    console.log(findIndex(arr));
