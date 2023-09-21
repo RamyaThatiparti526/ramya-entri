863e59981ff2f2f3f17de51fba79933e3d87e9dd
Ramya <ramyathatiparti@gmail.com>
2023-09-21 13:56:58 +0530

    added fles

--- assignment2/day3q/21.js
+++ assignment2/day3q/21.js
@@ -7,5 +7,16 @@
 //     const inputArray2 = [3, 4, 5, 6, 7];
 //     const output = findUnique(inputArray1, inputArray2);
 //     console.log(output); // [1, 2, 6, 7]
-
-
+function findUnique(array1,array2){
+for(let i=0;i<array1.length;i++){
+    for(let j=i;j<array2.length;j++){
+        if(array1[i]!==array2[j])
+        return i;
+    }
+    elseif(array2[j]!==array1[i])
+    return j;
+   }
+}
+let array1=[1,2,3,4,5];
+let array2=[3,4,5,6,7];
+console.log(findUnique(array1,array2));
