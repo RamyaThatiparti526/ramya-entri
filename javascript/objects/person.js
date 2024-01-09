40f10b069da74cd7e8639a947f356f36f2e7c507
RamyaThatiparti526 <ramyathatiparti526@gmail.com>
2024-01-09 10:16:35 +0530

    add changes

--- javascript/objects/person.js
+++ javascript/objects/person.js
@@ -1,36 +1,40 @@
-const person={
-    name:"Ramya thatiparti",
-    dob:"11.08.1988.",
-    qualification:"B.tech",
-    job:" ",
-    emailid:"ramyathatiparti@gmail.com",
-    hobbies:
-    {
+const person = {
+  name: "Ramya thatiparti",
+  dob: "10.08.1998.",
+  qualification: "B.tech",
+  job: " ",
+  emailid: "ramyathatiparti@gmail.com",
+  hobbies: {
     sports: "badminton",
     listen: "music",
-   }
-    }
-    console.log("name: ",person.name);
-    console.log(person.dob);
-    console.log(person. qualification);
-    console.log(person.job);
-    console.log(person.emailid);
-    console.log(person.hobbies.sports);
-    console.log(person["name"]);
-    
-    person.fname="thatiparti";
-    console.log(person);
-    delete person.dob;
-    console.log(person);
+  },
+};
+console.log("name: ", person.name);
+console.log(person.dob);
+console.log(person.qualification);
+console.log(person.job);
+console.log(person.emailid);
+console.log(person.hobbies.sports);
+console.log(person["name"]);
 
-    const array1=[[1,2,3],[4,5,6],[7,8,9],{name:"Ramya thatiparti",
-        dob:"11.08.1988.",
-        qualification:"B.tech",
-        job:" ",
-        emailid:"ramyathatiparti@gmail.com",}];
-    console.log(array1[0]);
-    console.log(array1[3].name);
-   array1[3].lname="thatiparti";
-   console.log(array1);
+person.fname = "thatiparti";
+console.log(person);
+delete person.dob;
+console.log(person);
 
-
+const array1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  {
+    name: "Ramya thatiparti",
+    dob: "10.08.1998.",
+    qualification: "B.tech",
+    job: " ",
+    emailid: "ramyathatiparti@gmail.com",
+  },
+];
+console.log(array1[0]);
+console.log(array1[3].name);
+array1[3].lname = "thatiparti";
+console.log(array1);
